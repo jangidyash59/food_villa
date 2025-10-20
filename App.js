adff1274e9250ec3b2f6f058e91b05257c6d9b9c
jangidyash59 <yashstudyonly1234@gmail.com>
2025-10-20 16:59:52 +0530

    base code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 //import React from "react"
 import ReactDOM from "react-dom/client"
 const heading = (
-    <h1 key="100">Welcome to Food Villa</h1>
+    <h1 key="100">Welcome to Food Villa !</h1>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
